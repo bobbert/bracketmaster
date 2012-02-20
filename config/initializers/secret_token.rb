26a21c2437552153b7ff3ba7742e8edf110cf5ec
Robert Phelps <bobbert0476@gmail.com>
2012-02-20 14:19:51 -0500

    initial commit with mongomapper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bracketmaster::Application.config.secret_token = '15d13babcbcffccb1cda9e7cb5edb38a086e98444a1fe6cfb1dc184760ddff629a66f74a21fe1bd24de77fdd1047a036e5db8a1255dcb3a52abcce9bb38c5a73'
